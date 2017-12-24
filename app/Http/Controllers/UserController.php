d76ed0d06cd3f9e045a76a673bc021c0dea85076
huibo <huibo@ihuibo.cn>
2017-12-24 12:33:14 +0800

    t

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,8 @@ class UserController extends Controller
 
     public function update(UserRequest $request, User $user)
     {
+       dd($request->avatar);
+
        $user->update($request->all());
        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
